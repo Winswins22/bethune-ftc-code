d9cead2c23703378f753645ec6f7b101eb228671
Winswins22 <73000223+Winswins22@users.noreply.github.com>
2021-11-17 10:36:46 -0500

    initial

---
+++ org.firstinspires.ftc.teamcode/Main2.java
@@ -0,0 +1,98 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@TeleOp(name="Pushbot: Mecanum wheels", group="mode")
+
+public class Main2 extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    HardwarePushbot robot           = new HardwarePushbot();   // Use a Pushbot's hardware
+    double          clawOffset      = 0;                       // Servo mid position
+    final double    CLAW_SPEED      = 0.02 ;                   // sets rate to move servo
+
+    @Override
+    public void runOpMode() {
+        double left;
+        double right;
+        double drive;
+        double turn;
+        double max;
+
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello Driver");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // Run wheels in POV mode (note: The joystick goes negative when pushed forwards, so negate it)
+            // In this mode the Left stick moves the robot fwd and back, the Right stick turns left and right.
+            // This way it's also easy to just drive straight, or just turn.
+            drive = -gamepad1.left_stick_y;
+            turn  =  gamepad1.right_stick_x;
+
+            mecanumDrive_Cartesian(-gamepad1.right_stick_x, gamepad1.right_stick_y, gamepad1.left_stick_x);
+            }
+        }
+    public void mecanumDrive_Cartesian(double x, double y, double rotation)
+{
+    double wheelSpeeds[] = new double[4];
+
+    wheelSpeeds[0] = x + y - rotation;
+    wheelSpeeds[1] = -x + y + rotation;
+    wheelSpeeds[2] = -x + y - rotation;
+    wheelSpeeds[3] = x + y + rotation;
+
+    //normalize(wheelSpeeds);
+
+    robot.frontLeft.setPower(-wheelSpeeds[0]);
+    robot.frontRight.setPower(wheelSpeeds[1]);
+    robot.backLeft.setPower(-wheelSpeeds[2]);
+    robot.backRight.setPower(wheelSpeeds[3]);
+    
+    telemetry.addData("front Left", robot.frontLeft.getPower());
+    telemetry.addData("front Right", robot.frontRight.getPower()); 
+    telemetry.addData("back Left", robot.backLeft.getPower()); 
+    telemetry.addData("back Right", robot.backRight.getPower()); 
+    telemetry.addData("x y r", x + " " + y + " " + rotation);
+    telemetry.update();
+    
+}   //end mecanumDrive_Cartesian
+
+// private void normalize(double[] wheelSpeeds)
+// {
+//     double maxMagnitude = Math.abs(wheelSpeeds[0]);
+
+//     for (int i = 1; i < wheelSpeeds.length; i++)
+//     {
+//         double magnitude = Math.abs(wheelSpeeds[i]);
+
+//         if (magnitude > maxMagnitude)
+//         {
+//             maxMagnitude = magnitude;
+//         }
+//     }
+
+//     if (maxMagnitude > 1.0)
+//     {
+//         for (int i = 0; i < wheelSpeeds.length; i++)
+//         {
+//             wheelSpeeds[i] /= maxMagnitude;
+//         }
+//     }
+// }   //normalize
+}//end class
