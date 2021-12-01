b8d29d3db7cf447f72359183362c4a706ab6c943
SY-MAIN <xxsimonxx119@gmail.com>
2021-12-01 10:24:14 -0500

    resolve duck and intake

--- org.firstinspires.ftc.teamcode/Main2.java
+++ org.firstinspires.ftc.teamcode/Main2.java
@@ -6,15 +6,14 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-
-@TeleOp(name="Pushbot: Mecanum wheels", group="mode")
+@TeleOp(name = "Pushbot: Mecanum wheels", group = "mode")
 
 public class Main2 extends LinearOpMode {
 
     /* Declare OpMode members. */
-    HardwarePushbot robot           = new HardwarePushbot();   // Use a Pushbot's hardware
-    double          clawOffset      = 0;                       // Servo mid position
-    final double    CLAW_SPEED      = 0.02 ;                   // sets rate to move servo
+    HardwarePushbot robot = new HardwarePushbot(); // Use a Pushbot's hardware
+    double clawOffset = 0; // Servo mid position
+    final double CLAW_SPEED = 0.02; // sets rate to move servo
 
     @Override
     public void runOpMode() {
@@ -24,13 +23,14 @@ public class Main2 extends LinearOpMode {
         double turn;
         double max;
 
-        /* Initialize the hardware variables.
+        /*
+         * Initialize the hardware variables.
          * The init() method of the hardware class does all the work here
          */
         robot.init(hardwareMap);
 
         // Send telemetry message to signify robot waiting;
-        telemetry.addData("Say", "Hello Driver");    //
+        telemetry.addData("Say", "Hello Driver"); //
         telemetry.update();
 
         // Wait for the game to start (driver presses PLAY)
@@ -39,81 +39,165 @@ public class Main2 extends LinearOpMode {
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            // Run wheels in POV mode (note: The joystick goes negative when pushed forwards, so negate it)
-            // In this mode the Left stick moves the robot fwd and back, the Right stick turns left and right.
+            // Run wheels in POV mode (note: The joystick goes negative when pushed
+            // forwards, so negate it)
+            // In this mode the Left stick moves the robot fwd and back, the Right stick
+            // turns left and right.
             // This way it's also easy to just drive straight, or just turn.
             drive = -gamepad1.left_stick_y;
-            turn  =  gamepad1.right_stick_x;
+            turn = gamepad1.right_stick_x;
 
             mecanumDrive_Cartesian(-gamepad1.right_stick_x, gamepad1.right_stick_y, gamepad1.left_stick_x);
-            
-            if (gamepad1.right_trigger > 0.0) {
-                robot.duckWheel.setPower(-1); 
-            }
-            else if (gamepad1.left_trigger > 0.0) {
-                robot.duckWheel.setPower(1);
-            }
-            else {
-                robot.duckWheel.setPower(0);
-            }
-            
-            if (gamepad1.right_bumper) {
-                robot.intake.setPower(-1); 
-            }
-            else if (gamepad1.left_bumper) {
-                robot.intake.setPower(1);
-            }
-            else {
-                robot.intake.setPower(0);
-            }
-            
+
+            // if (gamepad1.right_trigger > 0.0) {
+            // robot.duckWheel.setPower(-1);
+            // } else if (gamepad1.left_trigger > 0.0) {
+            // robot.duckWheel.setPower(1);
+            // } else {
+            // robot.duckWheel.setPower(0);
+            // }
+
+            // if (gamepad1.right_bumper) {
+            // robot.intake.setPower(-1);
+            // } else if (gamepad1.left_bumper) {
+            // robot.intake.setPower(1);
+            // } else {
+            // robot.intake.setPower(0);
+            // }
+
+            AutoDuck();
+            AutoIntake();
+
         }
+    }
+
+    public void mecanumDrive_Cartesian(double x, double y, double rotation) {
+        double wheelSpeeds[] = new double[4];
+
+        wheelSpeeds[0] = x + y - rotation;
+        wheelSpeeds[1] = -x + y + rotation;
+        wheelSpeeds[2] = -x + y - rotation;
+        wheelSpeeds[3] = x + y + rotation;
+
+        // normalize(wheelSpeeds);
+
+        robot.frontLeft.setPower(-wheelSpeeds[0]);
+        robot.frontRight.setPower(wheelSpeeds[1]);
+        robot.backLeft.setPower(-wheelSpeeds[2]);
+        robot.backRight.setPower(wheelSpeeds[3]);
+
+        telemetry.addData("front Left", robot.frontLeft.getPower());
+        telemetry.addData("front Right", robot.frontRight.getPower());
+        telemetry.addData("back Left", robot.backLeft.getPower());
+        telemetry.addData("back Right", robot.backRight.getPower());
+        telemetry.addData("x y r", x + " " + y + " " + rotation);
+        telemetry.update();
+
+    } // end mecanumDrive_Cartesian
+
+    public void AutoDuck() {
+        int ticksToIncreaseBy;
+        if (gamepad1.a) {
+            // Slow for 50000tick
+            double Dpower = 0.5;
+
+            ticksToIncreaseBy = translatePowerToTicks(Dpower);
+            robot.duckWheel.setTargetPosition(robot.duckWheel.getCurrentPosition() + ticksToIncreaseBy);
+            robot.duckWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            robot.duckWheel.setPower(Dpower);
+
+            while (robot.duckWheel.isBusy()) {
+
+                // Display it for the driver.
+                telemetry.addData("Motors are running to Positions", "");
+                telemetry.addData("duckWheel progress",
+                        robot.duckWheel.getTargetPosition() - robot.duckWheel.getCurrentPosition());
+                telemetry.update();
+            }
+
+            // Fast for 100000tick
+            int Ipower = 1;
+
+            ticksToIncreaseBy = translatePowerToTicks(Ipower);
+            robot.duckWheel.setTargetPosition(robot.duckWheel.getCurrentPosition() + ticksToIncreaseBy);
+            robot.duckWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            robot.duckWheel.setPower(Ipower);
+
+            while (robot.duckWheel.isBusy()) {
+
+                // Display it for the driver.
+                telemetry.addData("Motors are running to Positions", "");
+                telemetry.addData("duckWheel progress",
+                        robot.duckWheel.getTargetPosition() - robot.duckWheel.getCurrentPosition());
+                telemetry.update();
+            }
         }
-    public void mecanumDrive_Cartesian(double x, double y, double rotation)
-{
-    double wheelSpeeds[] = new double[4];
+        telemetry.addData("duckWheel progress", "done");
+    }
 
-    wheelSpeeds[0] = x + y - rotation;
-    wheelSpeeds[1] = -x + y + rotation;
-    wheelSpeeds[2] = -x + y - rotation;
-    wheelSpeeds[3] = x + y + rotation;
+    public void AutoIntake() {
+        int ticksToIncreaseBy;
+        if (gamepad1.b) {
+            // fast for 5000tick
+            int Dpower = 1;
 
-    //normalize(wheelSpeeds);
+            ticksToIncreaseBy = translatePowerToTicks(Dpower);
+            robot.intake.setTargetPosition(robot.intake.getCurrentPosition() + ticksToIncreaseBy);
+            robot.intake.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-    robot.frontLeft.setPower(-wheelSpeeds[0]);
-    robot.frontRight.setPower(wheelSpeeds[1]);
-    robot.backLeft.setPower(-wheelSpeeds[2]);
-    robot.backRight.setPower(wheelSpeeds[3]);
-    
-    telemetry.addData("front Left", robot.frontLeft.getPower());
-    telemetry.addData("front Right", robot.frontRight.getPower()); 
-    telemetry.addData("back Left", robot.backLeft.getPower()); 
-    telemetry.addData("back Right", robot.backRight.getPower()); 
-    telemetry.addData("x y r", x + " " + y + " " + rotation);
-    telemetry.update();
-    
-}   //end mecanumDrive_Cartesian
+            robot.intake.setPower(Dpower);
 
-// private void normalize(double[] wheelSpeeds)
-// {
-//     double maxMagnitude = Math.abs(wheelSpeeds[0]);
+            while (robot.intake.isBusy()) {
 
-//     for (int i = 1; i < wheelSpeeds.length; i++)
-//     {
-//         double magnitude = Math.abs(wheelSpeeds[i]);
+                // Display it for the driver.
+                telemetry.addData("Motors are running to Positions", "");
+                telemetry.addData("Intake progress",
+                        robot.intake.getTargetPosition() - robot.intake.getCurrentPosition());
+                telemetry.update();
+            }
+        }
+        telemetry.addData("Intake progress", "done");
+    }
 
-//         if (magnitude > maxMagnitude)
-//         {
-//             maxMagnitude = magnitude;
-//         }
-//     }
+    public void initMotor() {
+        robot.duckWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-//     if (maxMagnitude > 1.0)
-//     {
-//         for (int i = 0; i < wheelSpeeds.length; i++)
-//         {
-//             wheelSpeeds[i] /= maxMagnitude;
-//         }
-//     }
-// }   //normalize
-}//end class
+        // reset target positions
+        robot.duckWheel.setTargetPosition(robot.frontLeft.getCurrentPosition());
+
+        robot.duckWheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public int translatePowerToTicks(int speed) {
+        return (int) (speed * 10000);
+    }
+
+    public int translatePowerToTicks(double speed) {
+        return (int) (speed * 5000);
+    }
+
+    // private void normalize(double[] wheelSpeeds)
+    // {
+    // double maxMagnitude = Math.abs(wheelSpeeds[0]);
+
+    // for (int i = 1; i < wheelSpeeds.length; i++)
+    // {
+    // double magnitude = Math.abs(wheelSpeeds[i]);
+
+    // if (magnitude > maxMagnitude)
+    // {
+    // maxMagnitude = magnitude;
+    // }
+    // }
+
+    // if (maxMagnitude > 1.0)
+    // {
+    // for (int i = 0; i < wheelSpeeds.length; i++)
+    // {
+    // wheelSpeeds[i] /= maxMagnitude;
+    // }
+    // }
+    // } //normalize
+}// end class
